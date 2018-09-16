a9d23bc8c768b1417bcf83c579d52a0abdf64edd
Yundo Han <han@yun.do>
2018-09-16 14:56:08 +0900

    set json defaults to route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ require_dependency "discourse_plugin_template_constraint"
 constraints = DiscoursePluginTemplateConstraint.new
 DiscoursePluginTemplate::Engine.routes.draw do
   get "/hello" => "hello#index", constraints: constraints
-  get "/test" => "hello#test", constraints: constraints
+  get "/test" => "hello#test", constraints: constraints, defaults: { format: :json }
 end
